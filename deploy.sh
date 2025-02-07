9999f492279e8aff849bd0dda58e274f05685aea
rivaldo766hi <rivaldo.silalahi@gdplabs.id>
2025-02-07 08:42:40 +0700

    chore: init cicd

---
+++ deploy.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+set -xe
+
+# shellcheck source=/dev/null
+source /root/check.sh
+
+REPO_URL="https://github.com/GDP-ADMIN/${PROJECT}.git"
+BRANCH="${BRANCH_NAME}"
+CLONE_DIR="/home/deployer/${PROJECT}"
+
+echo "Setup git authentication"
+ssh deployer@"${TELEPORT_HOSTNAME}" <<EOF
+  git config --unset credential.helper
+  git config --global user.username infra-gl
+  git config --global user.email infra@gdplabs.id
+  git config --global url."https://${GH_TOKEN}:x-oauth-basic@github.com/".insteadOf "https://github.com/"
+
+  # Clone repo if not already present
+  if [ ! -d "${CLONE_DIR}" ]; then
+    git clone --branch "${BRANCH}" "${REPO_URL}" "${CLONE_DIR}"
+    cd "${CLONE_DIR}"
+    source env/bin/activate
+    python swirl.py restart
+  else
+    cd "${CLONE_DIR}"
+    git fetch origin
+    git checkout "${BRANCH}"
+    git pull origin "${BRANCH}"
+    source env/bin/activate
+    python swirl.py restart
+  fi
+
+EOF
+
